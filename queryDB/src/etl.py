e65fa44e8889a6d4de94c89d52f67356a55e9f64
manucubillo <manuel.cubillo1@gmail.com>
2022-10-13 22:00:21 +0200

    new docker to query notion db and generate monthly reports - github action to deploy dockers image to ecr

--- queryDB/src/etl.py
+++ queryDB/src/etl.py
@@ -1,75 +1,45 @@
+from dataclasses import dataclass
 import json
 from multiprocessing import Event
+from sqlite3 import Timestamp
 from urllib import response
 import urllib3
 import configparser
+import time
+import calendar
 from  schema import getSchemaTEST, getSchemaPROv1
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from pytz import timezone
+from dataclasses import dataclass
+import logging
 
-def extract_Structured(events, schema):
+logging.basicConfig(filename='log.log', encoding='utf-8', filemode="w", level=logging.INFO)
+
+@dataclass
+class item:
     """
-    check every parameters is in the message
+    item object in notion DB
     """
+    name:str = ""
+    date:datetime = ""
+    expenses:str = ""
+    month:str = ""
+    year:str = ""
+    day:str = ""
 
-    for key in json.loads(schema):
-        if not key in events.keys(): 
-            raise Exception("Sorry, missing values in message, or wrong key {} not in the message".format(key))
-
-
-    #get current date in format yyyy-mm-dd
-    fmt = "%Y-%m-%d"
-    now_time = datetime.now(timezone('Europe/Madrid'))
-    events['date'] = now_time.strftime(fmt)
-
-    #get categories
-    cat = events['Categories']
-    if len(cat) > 0: 
-        categories_string = ""
-        
-        for index, category in enumerate(cat):
-            categories_string += """{ "name" : \"""" + category + """\" }"""
-            
-            if (len(cat) - 1) > index:
-                categories_string += ","
-        
-        events['Categories'] = categories_string
-
-    else:
-        raise Exception("Category field missing value.")
-
-    # parse expenses
-    exp = events['Expenses']
-    exp.replace(',','.') # notion use dot for decimals
-    events['Expenses'] = exp
-
-    return events
-
-def extract_Not_Structured(events, schema):
+@dataclass
+class weekReport:
     """
-    not implemented yet
-    
+    summary of week
     """
-    raise Exception("Sorry, not structured data is not implemented yet")
+    month : str = ''
+    weekIni : str = ''
+    weekEnd : str = ''
+    totalAmount : int = 0
 
-def transform(db_id, params, schemaPro = False):
+def fetch_data(url, token, iniDate, endDate):
     """
-    build body message
-    """
-
-    if schemaPro:
-        body = getSchemaPROv1(db_id, params)
-    else:
-        body = getSchemaTEST(db_id, params)
-
-    return body
-
-def load(url, token, body):
-    """
-    send to Notion API the request
-
-    Returns:
-        result of the request
+    fetch data from notion db
     """
 
     headers = {'Notion-Version': '2021-08-16',
@@ -80,35 +50,256 @@ def load(url, token, body):
     http = urllib3.PoolManager()
     response = http.request('POST',
                             url,
-                            body = body,
+                            body = get_Date_Filter(iniDate, endDate),
                             headers = headers)
 
     dataJson = json.loads(response.data.decode('utf8'))
-    return dataJson
+    logging.debug(dataJson)
+    return dataJson['results']
 
+def get_Date_Filter(iniDate, endDate):
+    """
+    create date filter to fetch data from notion
+    """
+    week_day = endDate.weekday()
+    day = endDate.day -1
+    toD = endDate.strftime('%Y-%m-%d')
+
+    fromD = (endDate - timedelta(days=day)).strftime('%Y-%m-%d') # get month
+    
+    return str("""{
+    "filter": {
+          "and": [
+              {
+                  "property": "date",
+                  "date": {
+                      "on_or_before": \"""" + toD + """\"
+                  }
+              },
+              {
+                  "property": "date",
+                  "date": {
+                      "on_or_after": \"""" + fromD + """\"
+                  }
+              }
+          ]
+      }
+    }""")
+
+def format_data(data):
+    """
+    transform data into a known format
+    """
+    new_format = []
+
+    for d in data:
+        if len(d['properties']['item']['title']) == 0: # skip rows without name
+            continue
+        
+        i = item()
+        i.name = d['properties']['item']['title'][0]['plain_text']
+        i.expenses = d['properties']['Expenses']['number']
+        dateParts = d['properties']['date']['date']['start'].split('-')
+        
+        if len(dateParts) < 2: # if there is not date, skip this data
+            continue
+
+        i.date = datetime.fromisoformat( d['properties']['date']['date']['start'])
+        i.month = dateParts[1]
+        i.year = dateParts[0]
+        i.day = dateParts[2]
+
+        new_format.append(i)
+        
+
+    return data, new_format
+
+def group_by_week(data_formatted, iniDate, endDate):
+    """
+    take a formated data and work out the expenses by week
+    """
+    month_report = [] # list of weeks reports
+
+    current_date = endDate
+    current_month = current_date.strftime('%m')
+    current_year = current_date.strftime('%y')
+    current_day =  current_date.strftime("%d")
+    week_day = current_date.weekday()
+
+    # first iter
+    fromDate = current_date - timedelta(days=week_day)
+    toDate = current_date
+    totalAmount = getExpensesBetweenDays(data_formatted, fromDate, toDate)
+
+    wk = create_week_report(current_month, fromDate, toDate, totalAmount)
+
+    month_report.append(wk)
+
+    # others iter -  stop when month change
+    # number day of month - number day of week -> if there are more days, keep processing
+    daysToProcess = int(fromDate.strftime("%d")) - 1
+    if(daysToProcess > 0):
+        
+        offset_toDay = get_offSetToDay(toDate) # move toDate to the nearest Sunday
+
+        # work out if there is more than one week, or there are not enough days to make a full week
+        while(daysToProcess > 0):
+            wk = weekReport()
+
+            if(daysToProcess >= 7):
+                fromDate = fromDate - timedelta(days=7)
+                toDate =  toDate - timedelta(days=offset_toDay)
+                daysToProcess = daysToProcess - 7
+            else:
+                fromDate = fromDate - timedelta(days=daysToProcess)
+                toDate =  toDate - timedelta(days=offset_toDay)
+                daysToProcess = 0
+
+            offset_toDay = 7 # move toDate 7 days
+
+
+            totalAmount = getExpensesBetweenDays(data_formatted, fromDate , toDate )
+            wk = create_week_report(current_month, fromDate, toDate, totalAmount)
+            month_report.append(wk)
+
+    return month_report
+
+def create_week_report(current_month, fromDate, toDate, totalAmount):
+    """
+    week report constructor
+
+    Args:
+        current_month (_type_): _description_
+        fromDate (_type_): _description_
+        toDate (_type_): _description_
+        totalAmount (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    wk = weekReport()
+    wk.month = current_month
+    wk.weekIni = fromDate
+    wk.weekEnd = toDate
+    wk.totalAmount = totalAmount
+    return wk
+
+def getTotalMonth(m_report):
+    """
+    return total expense amount of the month
+
+    Args:
+        m_report (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    total = 0
+
+    for w in m_report:
+        total = total + w.totalAmount
+
+    return total
+
+def get_offSetToDay(toDate):
+    """
+    datetime.date consider monday as 1
+    """
+    if(toDate.isoweekday() == 1):
+        return 1
+    else:
+        return toDate.isoweekday()
+
+def getExpensesBetweenDays(data_formatted, fromDate: datetime, toDate: datetime):
+    """
+    calcule the sum of expenses those are between a date range. 
+    ini and end date are also taken into account
+
+    Args:
+        data_formatted (_type_): _description_
+        fromDate (datetime): _description_
+        toDate (datetime): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    amount = 0
+
+    for d in data_formatted:
+        logging.debug(d.name + " " + d.date.strftime("%m/%d/%Y")  + " " + str(d.expenses))
+
+    size = len(data_formatted)-1
+
+    result = filter(lambda x : datetime.timestamp(x.date) >= time.mktime(fromDate.timetuple()) and 
+    datetime.timestamp(x.date) <= time.mktime(toDate.timetuple()), data_formatted)        
+    result = list(result)
+    
+    if len(result) > 0:
+        for x in result:
+            amount = amount + int(x.expenses)
+
+
+    return amount
+
+def getDateRange(month):
+    """
+    given a month return the first and last day in datetime format
+    if it is the current month, return today as endDate
+    Args:
+        month (_type_): _description_
+    """
+
+    year = date.today().strftime("%Y")
+    lastDay = calendar.monthrange(int(year), int(month))[1]
+    d = "1/"+str(month)+"/"+year
+    ini = datetime.strptime(d, '%d/%m/%Y')
+    d = str(lastDay)+"/"+str(month)+"/"+year
+    end = datetime.strptime(d, '%d/%m/%Y')
+
+    if(date.today().strftime("%m") == month):
+        end = date.today()
+
+    return ini, end
+
+def create_message(m_report):
+    """
+    make a string message from the report
+    """
+
+    msg = ""
+    msg += "\n**** WEEKLY REPORT **** \n"
+
+    for week in m_report:
+        msg += "Report from " + week.weekIni.strftime("%d/%m/%Y") + "\n"
+        msg += "To... " + week.weekEnd.strftime("%d/%m/%Y") + "\n"
+        msg += "Total week -> " + str(week.totalAmount) + "\n"
+        msg += "+-+-+-+-+-+-+-+-+-+-+ \n"
+
+    msg += "-> TOTAL MONTH: " + str(getTotalMonth(m_report)) + " <-\n"
+    msg += "----------------------"
+
+    return msg
 
 def handler(event, context):
+
     config = configparser.ConfigParser()
     config.read('config.cfg')
-    
-    flagPro = config.get('DATA','SCHEMA') == 'PRO'
 
-    if flagPro:
-        db_key = config.get('KEYS','DATABASE_ID_PRO')
-    else:
-        db_key = config.get('KEYS','DATABASE_ID_TEST')
+    month = event['monthQuery']
 
-    structuredFlag = event['StructuredData'] == True
-
-    if structuredFlag: 
-        params = extract_Structured(event, config.get('DATA','DATA_SCHEMA'))
-    else:
-        params = extract_Not_Structured(event, config.get('DATA','DATA_SCHEMA'))
-
-    body = transform(db_key, params, schemaPro=flagPro)
-    response = load(config.get('URLS','ENDPOINT'), config.get('KEYS','BEARER_TOKEN'), body)
+    # 0. get date range depeding of the month 
+    iniDate, endDate = getDateRange(month)
+    # 1. download data from notion DB, for a delimited period of time
+    data = fetch_data(config.get('URLS','ENDPOINT'), config.get('KEYS','BEARER_TOKEN'), iniDate, endDate)
+    # 2. parse information from db to a item object 
+    data, data_formatted = format_data(data)
+    # 3. group the data by week to show the results
+    m_report = group_by_week(data_formatted, iniDate, endDate)
+    # 4. make a string message from the report
+    msg = create_message(m_report)
+    logging.info(msg)
 
     return {
         'statusCode': 200,
-        'body': response
+        'body': msg
     }
